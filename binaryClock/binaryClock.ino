5ae9d8eff011eb507c2d6021c6e0a4aaf33c2643
fidepus <mail@fidepus.de>
2014-12-20 01:33:32 +0100

    Fixed problem with pins.

--- binaryClock/binaryClock.ino
+++ binaryClock/binaryClock.ino
@@ -2,21 +2,26 @@ int second=0;
 int minute=0;
 int sunit=0;
 int munit=0;
+long previousMillis = 0;
+long interval = 1000;
 
 void setup() {
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-static unsigned long Tick = 0;
-
-  if (millis() - Tick >= 1000) 
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > interval) 
   {
-    Tick = millis();
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
     second++;
   }
   
@@ -27,18 +32,18 @@ static unsigned long Tick = 0;
   }
   
   sunit = second%10;
-  sunit = minute%10;
+  munit = minute%10;
 
  //second units
   if(sunit == 1 || sunit == 3 || sunit == 5 || sunit == 7 || sunit == 9) {  digitalWrite(1, LOW);} else {  digitalWrite(1, HIGH);}
-  if(sunit == 2 || sunit == 3 || sunit == 6 || sunit == 7) {digitalWrite(2, LOW);} else {digitalWrite(2,HIGH);}
-  if(sunit == 4 || sunit == 5 || sunit == 6 || sunit == 7) {digitalWrite(3, LOW);} else {digitalWrite(3,HIGH);}
-  if(sunit == 8 || sunit == 9) {digitalWrite(4, LOW);} else {digitalWrite(4,HIGH);}
+  if(sunit == 2 || sunit == 3 || sunit == 6 || sunit == 7) {digitalWrite(3, LOW);} else {digitalWrite(3,HIGH);}
+  if(sunit == 4 || sunit == 5 || sunit == 6 || sunit == 7) {digitalWrite(5, LOW);} else {digitalWrite(5,HIGH);}
+  if(sunit == 8 || sunit == 9) {digitalWrite(7, LOW);} else {digitalWrite(7,HIGH);}
 
   //seconds 
-  if((second >= 10 && second < 20) || (second >= 30 && second < 40) || (second >= 50 && second < 60))  {digitalWrite(5, LOW);} else {digitalWrite(5,HIGH);}
-  if(second >= 20 && second < 40)  {digitalWrite(6, LOW);} else {digitalWrite(6,HIGH);}
-  if(second >= 40 && second < 60) {digitalWrite(7, LOW);} else {digitalWrite(7,HIGH);}
+  if((second >= 10 && second < 20) || (second >= 30 && second < 40) || (second >= 50 && second < 60))  {digitalWrite(9, LOW);} else {digitalWrite(9,HIGH);}
+  if(second >= 20 && second < 40)  {digitalWrite(11, LOW);} else {digitalWrite(11,HIGH);}
+  if(second >= 40 && second < 60) {digitalWrite(13, LOW);} else {digitalWrite(13,HIGH);}
 
   
 }
